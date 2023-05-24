33944964d791524a78f95488adff812c361f1f20
Filipe Zabala <filipezabala@gmail.com>
2023-05-24 18:32:19 -0300

    Version 0.0.1.

--- export_E_sc.R
+++ export_E_sc.R
@@ -1,5 +1,10 @@
 # reading data
 Esc <- readr::read_csv('E_species_code.csv.gz')
+Ets <- readr::read_csv('E_test_soundscapes.csv.gz')
 
 #save to rda file
 save(Esc, file = './data/E_species_code.rda', compress = 'xz')
+save(Ets, file = './data/E_test_soundscapes.rda', compress = 'xz')
+
+devtools::install_github('filipezabala/birdclef2023')
+library(birdclef2023)
