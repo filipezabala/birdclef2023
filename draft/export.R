5ed0d5e6b596c61f5408178f7d62cf22bfd4aa47
Filipe Zabala <filipezabala@gmail.com>
2023-05-29 14:37:33 -0300

    Version 0.0.3.

--- draft/export.R
+++ draft/export.R
@@ -2,9 +2,15 @@
 Esc <- readr::read_csv('E_species_code.csv.gz')
 Ets <- readr::read_csv('E_test_soundscapes.csv.gz')
 
-#save to rda file
+# save to rda file
 save(Esc, file = './data/E_species_code.rda', compress = 'xz')
 save(Ets, file = './data/E_test_soundscapes.rda', compress = 'xz')
+
+# remove old packages
+# remove.packages('birdclef2023', lib='/usr/local/lib/R/site-library')
+
+# updating and creating manual
+devtools::document('~/MEGAsync/zabalab/kaggle/birdclef2023/')
 
 devtools::install_github('filipezabala/birdclef2023')
 library(birdclef2023)
